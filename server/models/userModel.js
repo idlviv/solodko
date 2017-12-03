bcb7432cb7e31050f43e28c3164256873c2dbf5b
idlviv <passpass1>
2017-12-03 22:28:12 +0200

    production v3

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -79,7 +79,13 @@ module.exports.addUser = function(newUser) {
           // якшо успішно записано в базу, то повертає в роутер відп обєкт
           newUser.save()
             .then(() => resolve({success: true, msg: 'Користувача зареєстровано'}))
-            .catch((error) => reject({success: false, msg: 'Не вдалося зареєструвати користувача'}))
+            .catch((error) => {
+              if (error.code === 11000) {
+                reject({success: false, msg: 'Ім\'я користувача або enail вже існує'})
+              } else {
+                reject({success: false, msg: 'Не вдалося зареєструвати користувача'})
+              }
+            });
         })
       .catch((error) => {throw error;});
   });
