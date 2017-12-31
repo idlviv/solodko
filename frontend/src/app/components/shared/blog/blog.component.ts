f1da675723e6369bd9afeb11ccf526487e09fedf
idlviv <idlviv@gmail.com>
2017-12-31 16:48:55 +0200

    +

--- frontend/src/app/components/shared/blog/blog.component.ts
+++ frontend/src/app/components/shared/blog/blog.component.ts
@@ -7,11 +7,24 @@ import {IBlog} from '../../../interfaces/i-blog';
   styleUrls: ['./blog.component.scss']
 })
 export class BlogComponent implements OnInit {
-@Input() blog: IBlog;
+
+  @Input() blog: IBlog;
+  @Input() index: number;
+
+  orderImg: any;
+  orderText: any;
+
 
   constructor() { }
 
   ngOnInit() {
+    if (this.index % 2 === 0) {
+      this.orderImg = 'order-lg-1 order-1';
+      this.orderText = 'order-lg-2 order-2';
+    } else {
+      this.orderImg = 'order-lg-2 order-1';
+      this.orderText = 'order-lg-1 order-2';
+    }
   }
 
 }
